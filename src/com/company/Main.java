9ba0f2725d3c3f43b629860f2f3194067c404cab
KERRCAM <kerrcameron@hotmail.com>
2021-09-22 21:28:03 +0100

    works bar employee name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,31 +13,39 @@ public class Main {
     double extraPay = 0.0;
     double totalPay = 0.0;
     double payAfterTax = 0.0;
-
     Scanner input = new Scanner(System.in);
-    System.out.println("what is the name of the employee?: ");
-    String employeeName = input.nextLine();
-    System.out.println("how many hours have they worked this week?: ");
-    double hoursWorked = input.nextDouble();
-    System.out.println("what is their hourly pay?: ");
-    double hourlyPay = input.nextDouble();
+    System.out.println("how many workers do you want to calculate pay for?: ");
+    int workers = input.nextInt();
+    int runs = workers;
+    boolean loopComplete = false;
+    while (loopComplete == false) {
+        System.out.println("what is the name of the employee?: ");
+        String employeeName = input.nextLine();
+        System.out.println("how many hours have they worked this week?: ");
+        double hoursWorked = input.nextDouble();
+        System.out.println("what is their hourly pay?: ");
+        double hourlyPay = input.nextDouble();
+        //wnd of getting variables/inputs
 
-    if (hoursWorked > 45.0) {
-        basePay = 45.0*hourlyPay;
-        extraPay = ((hoursWorked - 45.0) * (hourlyPay*1.5));
-        totalPay = basePay + extraPay;
+        if (hoursWorked > 45.0) {
+            basePay = 45.0 * hourlyPay;
+            extraPay = ((hoursWorked - 45.0) * (hourlyPay * 1.5));
+            totalPay = basePay + extraPay;
+        }
+        if (hoursWorked >= 0 && hoursWorked < 45) {
+            totalPay = hoursWorked * hourlyPay;
+        } else {
+            System.out.println("invalid input");
+        }
+        payAfterTax = totalPay * 0.8;
+        System.out.println("employee name: " + employeeName);
+        System.out.println("employee pay before tax: " + totalPay);
+        System.out.println("employee pay after tax: " + payAfterTax);
+        runs = runs-1;
+        if (runs == 0) {
+            loopComplete = true;
+        }
     }
-    if (hoursWorked >= 0 && hoursWorked < 45) {
-        totalPay = hoursWorked * hourlyPay;
-    }
-    else {
-        System.out.println("invalid input");
-    }
-    payAfterTax = totalPay * 0.8;
-    System.out.println("employee name: " + employeeName);
-    System.out.println("employee pay before tax: " + totalPay);
-    System.out.println("employee pay after tax: " + payAfterTax);
-    
 
 
 
